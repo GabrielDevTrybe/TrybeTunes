67ac63164bcaab7ecf9b9ab9c128b626ed3173b1
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-30 19:00:56 -0300

    requisito 3 feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,32 @@
-// import React from 'react';
+import React from 'react';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
+
+class Header extends React.Component {
+  state = {
+    isLoading: true,
+    name: '',
+
+  };
+
+  async componentDidMount() {
+    const user = await getUser();
+
+    this.setState({
+      name: user.name,
+      isLoading: false,
+    });
+  }
+
+  render() {
+    const { isLoading, name } = this.state;
+    return (
+      <header data-testid="header-component">
+        Header
+        {isLoading ? <Carregando /> : <h2 data-testid="header-user-name">{ name }</h2>}
+      </header>
+    );
+  }
+}
+
+export default Header;
