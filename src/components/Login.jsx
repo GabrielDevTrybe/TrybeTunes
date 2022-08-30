748a0199ea8a7b0b59c3486ca66f0b2920dbf8c3
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-30 15:54:03 -0300

    requisito 1 feito

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <h1>TrybeTunes</h1>
+        <p>Login</p>
+      </div>
+    );
+  }
+}
+
+export default Login;
