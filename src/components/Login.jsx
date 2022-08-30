7abbfccc9d6ca5aba4cc3500eebc5fe49c92af16
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-30 18:20:58 -0300

    requisito 2 feito

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  state = {
+    submitDisable: true,
+    name: '',
+    isLoading: false,
+    redirect: false,
+  };
+
+  funcButtonValue = (event) => {
+    const { name } = this.state;
+    const maxnLenghth = 2;
+
+    this.setState({ name: event.target.value });
+    if (name.length >= maxnLenghth) {
+      this.setState({
+        submitDisable: false,
+      });
+    } else if (name.length < maxnLenghth) {
+      this.setState({
+        submitDisable: true,
+      });
+    }
+  };
+
+  funcSaveInput = async (param) => {
+    param.preventDefault();
+
+    const { name } = this.state;
+
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+      redirect: true,
+    });
+  };
+
   render() {
+    const { submitDisable, isLoading, redirect } = this.state;
+
     return (
-      <div data-testid="page-login">
-        <h1>TrybeTunes</h1>
-        <p>Login</p>
-      </div>
+      isLoading ? <Carregando />
+        : (
+          <div data-testid="page-login">
+            {redirect && <Redirect to="/search" /> }
+            <h1>TrybeTunes</h1>
+            <form>
+              <label htmlFor="Login">
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  placeholder="Nome"
+                  onChange={ this.funcButtonValue }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="submit"
+                disabled={ submitDisable }
+                onClick={ this.funcSaveInput }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        )
     );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  submitDisable: PropTypes.bool,
+}.isRequired;
