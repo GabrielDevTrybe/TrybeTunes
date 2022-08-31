1bb962dc076eba77c2783930f7e1b1c6b45a89c0
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-08-31 14:55:11 -0300

    requisito 5 feeito

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  state = {
+    botaoOff: true,
+    searchValue: '',
+  };
+
+  botaoDisableFunc = (event) => {
+    const { searchValue } = this.state;
+    const maxLength = 1;
+
+    this.setState({ searchValue: event.target.value });
+    if (searchValue.length >= maxLength) {
+      this.setState({ botaoOff: false });
+    } else {
+      this.setState({ botaoOff: true });
+    }
+  };
+
   render() {
+    const { botaoOff } = this.state;
     return (
       <div data-testid="page-search">
-        <p>Search</p>
         <Header />
+        <form>
+          <input
+            onChange={ this.botaoDisableFunc }
+            type="text"
+            placeholder="search"
+            data-testid="search-artist-input"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ botaoOff }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
